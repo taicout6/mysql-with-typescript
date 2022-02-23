881326b7fdc30cdc650fadc017cc1f6b1a2a8012
taicout6 <taicout6@gmail.com>
2022-02-23 17:20:30 -0300

    Basic Model 2.0

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,37 +1,43 @@
 import { ResultSetHeader } from "mysql2";
 import connection from "./connection";
 
-interface IUser {
-  username: string,
-  email: string,
-  password: string,
-}
+import { IUser, User } from "./interfaces/IUser";
 
-interface User extends IUser {
-  id: number;
-}
-
-const getAll = async (): Promise<User[]> => {
+const getAllUsers = async (): Promise<User[]> => {
   const [rows] = await connection.execute('SELECT * FROM Users');
   return rows as User[];
 };
 
-const getById = async (id: number): Promise<User> => {
-  const [rows] = await connection.execute('SELECT * FROM Users WHERE id = ?', [id]);
+const getUserById = async (id: number): Promise<User> => {
+  const [rows] = await connection.execute('SELECT * FROM Users WHERE id=?', [id]);
   const [user] = rows as User[];
   return user;
 };
 
-const create = async (user: IUser): Promise<User> => {
+const createUser = async (user: IUser): Promise<User> => {
   const { username, email, password } = user;
   const query = 'INSERT INTO Users (username, email, password) VALUE (?, ?, ?)';
   const [result] = await connection.execute<ResultSetHeader>(query, [username, email, password]);
   const { insertId: id } = result;
   return { id, username, email, password };
-}
+};
+
+const updateUser = async (id: number, user: IUser) => {
+  const { username, email, password } = user;
+  const query = 'UPDATE Users SET username=?, email=?, password=? WHERE id=?';
+  const result = await connection.execute(query, [username, email, password, id]);
+  return { id, username, email, password };
+};
+
+const deleteUser = async (id: number): Promise<void> => {
+  await connection.execute('DELETE FROM Users WHERE id=?', [id]);
+  return;
+};
 
 export default {
-  getAll,
-  getById,
-  create,
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 }
